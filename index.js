75ce1f11af843a186e35609f1dd7fd26ada0cb5c
arman685913 <armanfarazi01@gmail.com>
2026-01-30 16:33:58 +0600

    user add to db

--- index.js
+++ index.js
@@ -24,14 +24,24 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+
+    const usersCollection = client.db("crowdcubeDB").collection("users");
+
+
+    app.post('/users' , async (req ,res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
