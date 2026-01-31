ad566b2b63e6ebda8c412f2959f436960e474259
arman685913 <armanfarazi01@gmail.com>
2026-01-31 17:15:05 +0600

    getSingleCampaign

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/campaigns/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id) }
+      const result = await campaignsCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post('/campaigns', async (req, res) => {
       const user = req.body;
       const result = await campaignsCollection.insertOne(user);
